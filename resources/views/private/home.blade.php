2306b384f7edb93f55b4744fd2d222235d4921af
juditblt <nagymiklosi.judit@blathy.info>
2022-05-10 17:57:05 +0200

    Create a new project.

---
+++ resources/views/private/home.blade.php
@@ -0,0 +1,33 @@
+@extends('private.layout')
+
+@section('content')
+    <h2>A belépett felhasználó ezt az oldalt látja először</h2>
+    <hr>
+    <h3>Itt lesznek a kategóriákba rendezett receptek</h3>
+    <hr>
+
+    <table>
+        <tr>
+            <th>Leves</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Szárnyasleves</td>
+            <td>A megmosott csirkeszárnyakat hidegvízben lassú tűzön főzni...</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Zöldségkrémleves</td>
+            <td>Párolt hagymára dobni a feldarabolt zöldségeket...</td>
+        </tr>
+        <tr>
+            <th>Köret</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Zöldségköret</td>
+            <td>Párolt hagymára dobni a feldarabolt zöldségeket...</td>
+        </tr>
+    </table>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\PrivateController;
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PublicController::class, 'index']);
+
+Route::get('/home', [PrivateController::class, 'home'])
+    ->name('private.home')->middleware(['auth']);
+
+require __DIR__.'/auth.php';
