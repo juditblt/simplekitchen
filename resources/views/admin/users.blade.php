501b9c5312d8d09cec18a824791fd1030add879d
juditblt <nagymiklosi.judit@blathy.info>
2022-05-24 11:22:21 +0200

    working on users.blade

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -14,14 +14,28 @@
             <th>Név</th>
             <th>Email</th>
             <th>Role</th>
-            <th></th>
+            <th>Adminná előléptet</th>
+            <th>Felhasználó törlése</th>
         </tr>
         @foreach($users as $user)
         <tr>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
             <td>{{ $user->role }}</td>
-            <td></td>
+            <td>
+                <form action="{{ route('admin.users.promote') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $user->id }}">
+                    <input type="submit" value="Előléptet">
+                </form>
+            </td>
+            <td>
+                <form action="{{ route('admin.users.delete') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $user->id }}">
+                    <input type="submit" value="Töröl">
+                </form>
+            </td>
         </tr>
         @endforeach
     </table>
