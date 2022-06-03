e5cc221393adda2a20cf961200fb80bc2172f691
juditblt <nagymiklosi.judit@blathy.info>
2022-06-03 13:15:15 +0200

    private home style

--- resources/views/private/home.blade.php
+++ resources/views/private/home.blade.php
@@ -1,40 +1,43 @@
 @extends('private.layout')
 
 @section('content')
-
     <hr>
+    <section class="row">
+        <div class="col-sm-12 col-md-8 m-2">
+            <h2>Üdvözöljük "{{ $user->name }}" ( {{ $user->email }} )!</h2>
 
-    <h2>Üdvözöljük "{{ $user->name }}" ( {{ $user->email }} )!</h2>
-    <hr>
+            @if($user->role == 'admin')
+                <a class="btn btn-primary" href="{{ route('admin') }}">Adminisztrátor kezdő oldal</a>
+            @endif
 
-    @if($user->role == 'admin')
-        <a href="{{ route('admin') }}">Adminisztrátor kezdő oldal</a>
-    @endif
 
-    <hr>
-    <a href="{{ route('private.create') }}">Új recept felvétele</a>
-    <hr>
+            <a class="btn btn-primary" href="{{ route('private.create') }}">Új recept felvétele</a>
+        </div>
+    </section>
 
-    <h3>Itt láthatja az összes receptjét:</h3>
-    <hr>
+    <section class="row justify-content-center">
+        <div class="col-sm-12 col-md-6">
+            <h3>Itt láthatja az összes receptjét:</h3>
 
-    <table>
-        <tr>
-            <th>Kategória név</th>
-            <th>Recept név</th>
-            <th>Publ(1)/Priv(0)</th>
-            <th>Részletek</th>
-        </tr>
-        @foreach($recipes as $recipe)
-            <tr>
-                <td>{{ $recipe->category->name }}</td>
-                <td>{{ $recipe->name }}</td>
-                <td>{{ $recipe->public }}</td>
-                <td>
-                    <a href="{{ route('private.details', ['id' => $recipe->id]) }}">Részletek</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
+            <table class="table table-sm">
+                <tr>
+                    <th>Kategória név</th>
+                    <th>Recept név</th>
+                    <th>Publ(1)/Priv(0)</th>
+                    <th>Részletek</th>
+                </tr>
+                @foreach($recipes as $recipe)
+                    <tr>
+                        <td>{{ $recipe->category->name }}</td>
+                        <td>{{ $recipe->name }}</td>
+                        <td>{{ $recipe->public }}</td>
+                        <td>
+                            <a href="{{ route('private.details', ['id' => $recipe->id]) }}">Részletek</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </section>
 
 @endsection
