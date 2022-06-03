e354067e9ed3e95435e7db9085f421511e6a7392
juditblt <nagymiklosi.judit@blathy.info>
2022-06-03 13:01:56 +0200

    new css file

--- resources/views/private/layout.blade.php
+++ resources/views/private/layout.blade.php
@@ -7,22 +7,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ \Illuminate\Support\Facades\URL::asset('css/home.css') }}">
     <title>Simplekitchen</title>
 </head>
 <body>
-    <h1>Egyszerű receptek</h1>
+    <main class="container">
+        <h1 class="row justify-content-center m-3">Egyszerű receptek</h1>
 
-    <form action="{{ route('logout') }}" method="post">
-        @csrf
-        <input type="submit" value="Kilépés">
-    </form>
+        <section class="row">
+            <div class="navbar navbar-expand-lg">
+                <ul class="navbar-nav">
+                    <li class="nav-item m-2">
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <input class="btn btn-primary" type="submit" value="Kilépés">
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </section>
 
-    @section('content')
-        private oldal tartalma...
-    @show
+        @section('content')
+            private oldal tartalma...
+        @show
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                crossorigin="anonymous"></script>
+    </main>
 </body>
 </html>
